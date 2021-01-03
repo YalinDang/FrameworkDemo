db4d49d8a8996b3fabf64a20e9839e42ea7aca93
党亚林 <dangyl@tech.icbc.com.cn>
2021-01-03 16:36:32 +0800

    增加XIDFramework

---
+++ SDK/XIDFramework.framework/Headers/XIDFramework.h
@@ -0,0 +1,19 @@
+//
+//  XIDFramework.h
+//  XIDFramework
+//
+//  Created by DangYalin on 2021/1/3.
+//
+
+#import <Foundation/Foundation.h>
+#import "LoginVC.h"
+
+//! Project version number for XIDFramework.
+FOUNDATION_EXPORT double XIDFrameworkVersionNumber;
+
+//! Project version string for XIDFramework.
+FOUNDATION_EXPORT const unsigned char XIDFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XIDFramework/PublicHeader.h>
+
+
